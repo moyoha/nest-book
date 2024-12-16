b4688381935135960056b9372be575a37cb4b1c9
v_mayonghua <v_mayonghua@baidu.com>
2024-12-16 19:01:29 +0800

    5. 5种常见的 http 数据传输方式

---
+++ src/person/person.controller.ts
@@ -0,0 +1,80 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  UseInterceptors,
+  UploadedFiles,
+} from '@nestjs/common';
+import { PersonService } from './person.service';
+import { CreatePersonDto } from './dto/create-person.dto';
+import { UpdatePersonDto } from './dto/update-person.dto';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
+
+@Controller('/api/person')
+export class PersonController {
+  constructor(private readonly personService: PersonService) {}
+
+  @Get('find')
+  query(@Query('name') name: string, @Query('age') age: number) {
+    return {
+      name,
+      age,
+    };
+  }
+
+  @Get(':id')
+  urlPram(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+
+  @Post()
+  body(@Body() createPersonDto: CreatePersonDto) {
+    return createPersonDto;
+  }
+
+  @Post('file')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      dest: 'uploads/',
+    }),
+  )
+  uploadFiles(
+    @Body() createPersonDto: CreatePersonDto,
+    @UploadedFiles() files: Array<Express.Multer.File>,
+  ) {
+    console.log(files);
+    return createPersonDto;
+  }
+
+  @Post()
+  create(@Body() createPersonDto: CreatePersonDto) {
+    return this.personService.create(createPersonDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.personService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.personService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePersonDto: UpdatePersonDto) {
+    return this.personService.update(+id, updatePersonDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.personService.remove(+id);
+  }
+}
